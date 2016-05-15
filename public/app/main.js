49e961b007a814dd1c4a8cd07d58dfac5e5b60f6
imears <maxwellmears@gmail.com>
2016-05-14 23:31:19 -0700

    Only show template component

--- public/app/main.js
+++ public/app/main.js
@@ -2,13 +2,10 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
 var http_1 = require('@angular/http');
-var employee_component_1 = require('./employee/employee.component');
-var giphy_component_1 = require('./giphy/giphy.component');
 var template_component_1 = require('./template/template.component');
-var youtube_component_1 = require('./youtube/youtube.component');
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
-platform_browser_dynamic_1.bootstrap(giphy_component_1.GiphyComponent, [http_1.HTTP_PROVIDERS]);
-platform_browser_dynamic_1.bootstrap(employee_component_1.EmployeeComponent, [http_1.HTTP_PROVIDERS]);
+// bootstrap(GiphyComponent, [HTTP_PROVIDERS]);
+// bootstrap(EmployeeComponent, [HTTP_PROVIDERS]);
 platform_browser_dynamic_1.bootstrap(template_component_1.TemplateComponent, [http_1.HTTP_PROVIDERS]);
-platform_browser_dynamic_1.bootstrap(youtube_component_1.YoutubeComponent, [http_1.HTTP_PROVIDERS]);
+// bootstrap(YoutubeComponent, [HTTP_PROVIDERS]);
 //# sourceMappingURL=main.js.map
--- public/app/main.ts
+++ public/app/main.ts
@@ -7,7 +7,7 @@ import {TemplateComponent} from './template/template.component';
 import {YoutubeComponent} from './youtube/youtube.component';
 
 bootstrap(AppComponent, [HTTP_PROVIDERS]);
-bootstrap(GiphyComponent, [HTTP_PROVIDERS]);
-bootstrap(EmployeeComponent, [HTTP_PROVIDERS]);
+// bootstrap(GiphyComponent, [HTTP_PROVIDERS]);
+// bootstrap(EmployeeComponent, [HTTP_PROVIDERS]);
 bootstrap(TemplateComponent, [HTTP_PROVIDERS]);
-bootstrap(YoutubeComponent, [HTTP_PROVIDERS]);
+// bootstrap(YoutubeComponent, [HTTP_PROVIDERS]);
