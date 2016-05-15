d839918b32da34bd70ce4a379cff6ecf49efb9eb
imears <maxwellmears@gmail.com>
2016-05-15 15:23:31 -0700

    Change refesh btn

--- public/app/template/template.component.ts
+++ public/app/template/template.component.ts
@@ -22,6 +22,7 @@ export class TemplateComponent {
   catchPhrase: string;
   bs: string;
   bsBuzz: string;
+  companies: any[];
 
   ngOnInit() {
     var self = this;
@@ -32,15 +33,17 @@ export class TemplateComponent {
 
     this._CompanyService.getCompany()
       .then(function(response) {
-        let companyData    = response.json();
-        console.log(companyData)
-        self.companyName   = companyData.companyName;
-        self.companySuffix = companyData.companySuffix;
-        self.catchPhrase   = companyData.catchPhrase;
-        self.bs            = companyData.bs;
-        self.bsBuzz        = companyData.bsBuzz;
+        let companies = response.json();
+        let company = companies[0];
+
+        self.companyName = company.companyName;
+        self.companySuffix = company.companySuffix;
+        self.catchPhrase = company.catchPhrase;
+        self.bs = company.bs;
+        self.bsBuzz = company.bsBuzz;
         self.primaryColor = ['#318dbc', '#fc4538', '#1469aa', '#fb8c18',  '#0e427f', '#5ac2cb', '#7ecca8', '#fed200','#cdeac6', '#febb60'][Math.floor(Math.random() * 10)]
         self.secondaryColor = ['#9b78ce', '#fecf60', '#9ee2fa', '#44b44e', '#b1248e', '#04aae4', '#e48134', '#abcb76', '#ed4a3c', '#ec008c'][Math.floor(Math.random() * 10)]
+        self.companies = companies;
       });
   }
 }
