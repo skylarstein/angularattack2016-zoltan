b6ab9ec3ff6de8b82b133ca0cf68d05a2ce03436
Skylar Stein <skylar@peringer.com>
2016-05-14 19:06:07 -0700

    cleanup employee service temporary code

--- public/app/employee/employee.service.ts
+++ public/app/employee/employee.service.ts
@@ -1,15 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http, HTTP_PROVIDERS} from '@angular/http';
 
-class Employee {
-  name: string;
-  jobTitle: string;
-  avatarUrl: string;
- 
-  constructor() { }
-  //toString(): string { }
-}
-
 @Injectable()
 export class EmployeeService {
   constructor(private http: Http) { }
@@ -17,20 +8,3 @@ export class EmployeeService {
     return this.http.get('random/employee');
   }
 }
-
-
-/*
-    this.http.get('random/employee')
-      .subscribe(function(result) {
-        console.log('>>>>>>' + JSON.stringify(result));
-
-        var employee = new Employee();
-
-        employee.name = 'Joe Blow';
-        employee.jobTitle = 'Software Guy';
-        employee.avatarUrl = 'http://my.image.com/hello.png';
-
-        console.log(employee);
-        return employee;
-    });
-*/
