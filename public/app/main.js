14be22296f18ffc81b1414d211c89b75a70f1e67
Skylar Stein <skylar@peringer.com>
2016-05-15 11:53:07 -0700

    Angular enableProdMode()

--- public/app/main.js
+++ public/app/main.js
@@ -3,6 +3,8 @@ var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
 var http_1 = require('@angular/http');
 var template_component_1 = require('./template/template.component');
+var core_1 = require('@angular/core');
+core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
 // bootstrap(GiphyComponent, [HTTP_PROVIDERS]);
 // bootstrap(EmployeeComponent, [HTTP_PROVIDERS]);
