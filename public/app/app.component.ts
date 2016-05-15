8f2413474602a789927948d3dea4466f70a0f65a
imears <maxwellmears@gmail.com>
2016-05-15 13:36:29 -0700

    Add catch phrase

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -7,19 +7,10 @@ import {EmployeeService} from './employee/employee.service';
 @Component({
   selector: 'my-app',
   template: '',
-  providers: [CompanyService, GiphyService, YoutubeService, EmployeeService]
+  providers: []
 })
 
 export class AppComponent {
-  constructor(private _CompanyService: CompanyService, private _GiphyService: GiphyService, private _YoutubeService: YoutubeService, private _EmployeeService: EmployeeService) {
-
-  }
-
-  getCompanyName() {
-    this._CompanyService.getDomainName().then(function(result: any) {});
-  }
-
-  ngOnInit() {
-    this.getCompanyName();
-  }
+  constructor() {}
+  ngOnInit() {}
 }
--- public/app/company/company.service.ts
+++ public/app/company/company.service.ts
@@ -5,18 +5,17 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class CompanyService {
-    constructor(private http: Http) { }
-    getName() {
-        return this.http.get('/random/words/2').toPromise();
-    }
-    getDomainName() {
-        var httpService = this.http;
-        return this.getName().then(function(result : any) {
-            result = result.json();
-            if (result.words) {
-                console.log('http://www.namemesh.com/domain-name-search/'+ result.words[0] + result.words[1] + '?show=1');
-                return httpService.get('http-get-proxy/' + encodeURIComponent('http://www.namemesh.com/domain-name-search/'+ result.words[0] + result.words[1] + '?show=1')).toPromise();
-            }
-        });
-    }
+  constructor(private http: Http) { }
+
+  getName() {
+    return this.http.get('/random/words/2').toPromise();
+  }
+
+  getCompany() {
+    return this.http.get('/random/company').toPromise();
+  }
+
+  getKantianSentences() {
+    return this.http.get('/random/sentences/10').toPromise();
+  }
 }
